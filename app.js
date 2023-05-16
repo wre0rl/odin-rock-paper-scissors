f206971130f745933893b7ec4fc4ab29f440fe07
wre0rl <wre0rl@gmail.com>
2023-05-16 15:53:25 +0700

    Showing score, player choice and event handler on the button for the html

--- app.js
+++ app.js
@@ -3,49 +3,82 @@ Array.prototype.random = function () {
 }
 
 function getComputerChoice() {
-  const el = ['rock', 'paper', 'scissors'];
+  const el = ['Rock', 'Paper', 'Scissors'];
   return el.random();
 }
 
 function playRound(playerSelection, computerSelection) {
   const outcomes = {
-    rock: 'scissors', // Rock beats Scissors
-    paper: 'rock', // Paper beats Rock
-    scissors: 'paper' // Scissors beats Paper 
+    Rock: 'Scissors', // Rock beats Scissors
+    Paper: 'Rock', // Paper beats Rock
+    Scissors: 'Paper' // Scissors beats Paper 
   };
 
   if (outcomes[computerSelection] === playerSelection) {
     computerScore += 1;
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
   } else if (outcomes[playerSelection] === computerSelection) {
     playerScore += 1;
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
   } else {
-    return 'Draw!';
+    playerScore += 1;
+    computerScore += 1;
+    console.log('Draw!');
   }
+}
+
+function showScore(playerScore, computerScore) {
+  const playerScoreContent = document.querySelector('.player-score-content');
+  const computerScoreContent = document.querySelector('.computer-score-content');
+
+  playerScoreContent.textContent = playerScore;
+  computerScoreContent.textContent = computerScore;
 }
 
 function showWinner(playerScore, computerScore) {
-  const finalScore = `Player: ${playerScore}\nComputer: ${computerScore}\n`
+
   if (playerScore > computerScore) {
-    return `${finalScore}Player is the final winner! Congrats!`;
+    return `Player is the final winner! Congrats!`;
   } else if (computerScore > playerScore) {
-    return `${finalScore}Computer is the final winner!`;
+    return `Computer is the final winner!`;
   } else {
-    return `${finalScore}The final result is draw!`;
+    return `The final result is draw!`;
   }
 }
 
+const btns = document.querySelectorAll('.rpc-button');
+const playerChoiceContent = document.querySelector('.player-choice');
+const computerChoiceContent = document.querySelector('.computer-choice');
+
 let playerScore = 0;
 let computerScore = 0;
+let round = 0;
+
+btns.forEach((btn) => {
+  btn.addEventListener('click', game);
+});
+
 function game() {
-  let round = 0;
-  const playerSelection = prompt('Pick your choice!');
+  // Get the player and computer selections
+  const playerSelection = this.id;
   const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection.toLowerCase(), computerSelection));
+
+  // Render player and computer choice text
+  playerChoiceContent.textContent = playerSelection;
+  computerChoiceContent.textContent = computerSelection;
+
+  // Play, update and show the score for each round
+  playRound(playerSelection, computerSelection);
+  showScore(playerScore, computerScore);
+
+  // Count and stop if the round is = 5
   round++;
-  console.log(showWinner(playerScore, computerScore));
+  if (round === 5) reset();
+  console.log(round);
 }
 
-game();
-
+function reset() {
+  round = 0; 
+  playerScore = 0; 
+  computerScore = 0
+}
